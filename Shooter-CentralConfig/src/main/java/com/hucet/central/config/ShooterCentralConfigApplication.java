7f2a9dfc6db6aafb0b6ed0d18a75f9b87b246578
taesu <kimtaesoo188@gmail.com>
2017-01-29 10:51:49 +0900

    Config Server 설정

--- Shooter-CentralConfig/src/main/java/com/hucet/central/config/ShooterCentralConfigApplication.java
+++ Shooter-CentralConfig/src/main/java/com/hucet/central/config/ShooterCentralConfigApplication.java
@@ -2,11 +2,15 @@ package com.hucet.central.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ShooterCentralConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShooterCentralConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShooterCentralConfigApplication.class, args);
+    }
 }
