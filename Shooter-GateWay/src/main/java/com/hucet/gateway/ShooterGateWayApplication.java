60f3f3770382198f61eb594a9990b251bc2266a8
taesu <kimtaesoo188@gmail.com>
2017-01-29 11:01:45 +0900

    gateway server

--- Shooter-GateWay/src/main/java/com/hucet/gateway/ShooterGateWayApplication.java
+++ Shooter-GateWay/src/main/java/com/hucet/gateway/ShooterGateWayApplication.java
@@ -2,11 +2,15 @@ package com.hucet.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ShooterGateWayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShooterGateWayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShooterGateWayApplication.class, args);
+    }
 }
