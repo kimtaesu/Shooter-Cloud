442a546504f2dcb84ed7d5c31c7fe7f870dc40af
taesu <kimtaesoo188@gmail.com>
2017-01-29 09:16:10 +0900

    Initial commit

---
+++ src/main/java/com/hucet/cloud/ShooterCloudApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooterCloudApplication.class, args);
+	}
+}
