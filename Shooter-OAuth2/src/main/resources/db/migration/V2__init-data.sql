b9caf00bc0245b93e7ffda7a24b695a3a8bf765a
taesu <kimtaesoo188@gmail.com>
2017-02-10 15:39:28 +0900

    flyway db 적용

--- Shooter-OAuth2/src/main/resources/db/migration/V2__init-data.sql
+++ Shooter-OAuth2/src/main/resources/db/migration/V2__init-data.sql
@@ -10,6 +10,20 @@ insert into oauth (
   password,
   user_name,
   user_email)
-  values (null, 1, 1, 1, 1, 'test', 'test1', 'test@testt1.coas');
+  values (null, 1, 1, 1, 1, 'test1', 'test1', 'test1@testtest.co.aa');
+
 
 insert into user_role (user_id, role_id) values (1, 2);
+
+insert into oauth (
+  oauth_id,
+  is_account_non_expired,
+  is_account_non_locked,
+  is_credentials_non_expired,
+  is_enabled,
+  password,
+  user_name,
+  user_email)
+  values (null, 1, 1, 1, 1, 'test2', 'test2', 'test2@testtest.co.aa');
+
+insert into user_role (user_id, role_id) values (2, 1);
--- Shooter-UserService/src/main/resources/db/migration/V2__init-data.sql
+++ Shooter-UserService/src/main/resources/db/migration/V2__init-data.sql
@@ -1,4 +1,5 @@
 insert into account (account_id, password, user_email, user_name)
-values
-(null, 'test1', 'test1@qweskdc.als', 'test1');
-(null, 'test2', 'test1@qweskdc.als', 'test2');
+values (null, 'test1', 'test1@testtest.co.aa', 'test1');
+
+insert into account (account_id, password, user_email, user_name)
+values (null, 'test2', 'test2@testtest.co.aa', 'test2');
