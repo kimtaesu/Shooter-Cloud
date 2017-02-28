13d228d8ae0bf3d73947fc5463cd2ddf1a2c1a07
taesu <kimtaesoo188@gmail.com>
2017-02-28 15:48:16 +0900

    회원가입 Form 작성

--- Shooter-WebFront/src/app/home/home.module.ts
+++ Shooter-WebFront/src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HomeComponent} from "./home.component";
 import {LoginComponent} from "../login/login.component";
 import {MaterialModule} from "@angular/material";
@@ -16,6 +16,7 @@ import {JoinComponent} from "../join/join.component";
     HomeRoutingModule,
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule.forRoot(),
   ],
   providers: [
--- Shooter-WebFront/src/app/join/join.component.ts
+++ Shooter-WebFront/src/app/join/join.component.ts
@@ -1,13 +1,34 @@
-import {Component} from "@angular/core";
-import {Router} from "@angular/router";
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, Validators, FormControl} from "@angular/forms";
+import {ValidationService} from "../shared/validator/validator";
 
 @Component({
   selector: 'app-join',
   templateUrl: 'join.component.html',
   styleUrls: ['join.component.css']
 })
-export class JoinComponent {
-  constructor(private _router: Router) {
+export class JoinComponent implements OnInit {
 
+  Form: FormGroup;
+  user = {
+    email: '',
+    userName: '',
+    password: '',
+  }
+
+  ngOnInit(): void {
+    this.Form = new FormGroup({
+      email: new FormControl('', Validators.compose([Validators.required, ValidationService.emailValidator])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        ValidationService.passwordValidator])),
+      userName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ])),
+    });
   }
 }
