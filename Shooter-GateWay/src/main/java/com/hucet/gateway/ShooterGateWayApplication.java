be6b5bd3ab2e40359e21a98420442b9b0229884d
taesu <kimtaesoo188@gmail.com>
2017-03-11 04:36:09 +0900

    docker 파일 추가.

--- Shooter-GateWay/src/main/java/com/hucet/gateway/ShooterGateWayApplication.java
+++ Shooter-GateWay/src/main/java/com/hucet/gateway/ShooterGateWayApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@RestController
 // Zuul 서버는 기본적으로 service name 을 통한 Loadbalance를 지원한다.
 public class ShooterGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShooterGateWayApplication.class, args);
+    }
+
+    @GetMapping
+    String hello()
+    {
+        return "Hello";
     }
 }
